b81d420d71d23f3b5ef2a6744ff82632da252cb1
Chantell Nichols <chantellnichols13@gmail.com>
2026-06-21 15:48:03 -0400

    Filter Coins

--- src/components/CoinCard.jsx
+++ src/components/CoinCard.jsx
@@ -14,7 +14,9 @@ const CoinCard = ({ coin }) => {
           coin.price_change_percentage_24h >= 0 ? 'positive' : 'negative'
         }
       >
-        {coin.price_change_percentage_24h.toFixed(2)}%
+        {coin.price_change_percentage_24h != null
+          ? `${coin.price_change_percentage_24h.toFixed(2)}%`
+          : 'N/A'}
       </p>
       <p>Market Cap: {coin.market_cap.toLocaleString()}</p>
     </div>
